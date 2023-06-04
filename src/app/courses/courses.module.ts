db5d589cde08a3939b26ceccd72c9225e36e3322
Sergio <sergioadm120@gmail.com>
2023-06-04 17:48:02 -0300

    refract: courses.module and directory shared

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from './../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
 import { CoursesComponent } from './containers/courses/courses.component';
 import { CourseFormComponent } from './containers/course-form/course-form.component';
