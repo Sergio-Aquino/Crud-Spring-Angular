39b89e4a15fd9f73447286c2973cc39349df3299
Sergio <sergioadm120@gmail.com>
2023-06-04 19:57:34 -0300

    feature: add CourseRepository

--- src/main/java/com/Sergio/model/Course.java
+++ src/main/java/com/Sergio/model/Course.java
@@ -1,29 +1,20 @@
 package com.Sergio.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
-import org.hibernate.validator.constraints.Length;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.Sergio.enums.Category;
 import com.Sergio.enums.Status;
 import com.Sergio.enums.converters.CategoryConverter;
 import com.Sergio.enums.converters.StatusConverter;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
